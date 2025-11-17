06d2b1b2f46044a7154ae59b42f1e40733d6c8ef
gitlab-ci-dianmife <gitlab-ci-dianmife@dianmi365.com>
2025-11-17 17:30:46 +0800

    fix 类型定义错误

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,3 +9,22 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+// Background Sync API 类型定义
+interface SyncManager {
+  getTags(): Promise<string[]>;
+  register(tag: string): Promise<void>;
+}
+
+interface ServiceWorkerRegistration {
+  readonly sync: SyncManager;
+}
+
+interface ExtendableEvent extends Event {
+  waitUntil(fn: Promise<unknown>): void;
+}
+
+interface SyncEvent extends ExtendableEvent {
+  readonly lastChance: boolean;
+  readonly tag: string;
+}
